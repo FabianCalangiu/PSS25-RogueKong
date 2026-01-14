024d5f84c0a64f372018977c835bdd360b5f162e
Fabian <fabian.calangiu@studio.unibo.it>
2026-01-14 17:03:18 +0100

    Add source file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,24 @@
+plugins {
+    java
+    application
+}
 
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+application {
+    mainClass.set("your.package.Main")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+}
