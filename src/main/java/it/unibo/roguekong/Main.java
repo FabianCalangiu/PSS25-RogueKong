bda1cab203f01219cf975b15ca9b656440716a15
Menghetz <manuelmenghetti0@gmail.com>
2026-01-15 14:29:13 +0100

    test commit

--- src/main/java/it/unibo/roguekong/Main.java
+++ src/main/java/it/unibo/roguekong/Main.java
@@ -4,5 +4,6 @@ public class Main {
     static void main() {
         System.out.println("Hello");
         System.out.println("World by Vampa");
+        System.out.println("Hello world by Manuel");
     }
 }
