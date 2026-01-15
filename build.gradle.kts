b73a2dfd3295f4ae76a6a7646f0e85b6dc7442e9
Fabian <fabian.calangiu@studio.unibo.it>
2026-01-15 17:23:40 +0100

    Fixed jfx dependencies || Add sample window.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     application
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 repositories {
@@ -13,30 +14,19 @@ java {
     }
 }
 
+javafx {
+    version = "25"
+    modules = listOf("javafx.controls", "javafx.fxml", "javafx.graphics")
+}
+
 application {
     mainClass.set("it.unibo.roguekong.Main")
 }
 
-tasks.jar {
-    manifest {
-        attributes["Main-Class"] = application.mainClass.get()
-    }
-}
-
-dependencies {
-    // JavaFX (versione compatibile)
-    val javafxVersion = "21"
-    val javafxModules = listOf("base", "controls", "graphics")
-
-    for (module in javafxModules) {
-        implementation("org.openjfx:javafx-$module:$javafxVersion")
-    }
-
-    // Test (opzionale)
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
 
 tasks.test {
-    useJUnitPlatform()
+    enabled = false
 }
