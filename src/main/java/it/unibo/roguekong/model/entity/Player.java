ee979ac5eea5fdba41fc5dbef523d9edde71c26e
evampa <evampa@imolinfo.it>
2026-01-15 15:42:13 +0100

    added interactable, powerup and game interfaces

--- src/main/java/it/unibo/roguekong/model/entity/Player.java
+++ src/main/java/it/unibo/roguekong/model/entity/Player.java
@@ -1,12 +1,14 @@
 package it.unibo.roguekong.model.entity;
 
+import it.unibo.roguekong.model.value.Position;
+import it.unibo.roguekong.model.value.Velocity;
 import java.util.List;
 
 /**
  * This interface is going to represent player
  */
 
-public interface Player {
+public interface Player{
     public Position getPosition();
 
     public Velocity getVelocity();
