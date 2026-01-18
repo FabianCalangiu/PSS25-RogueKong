6de322f8552113dbc216977ca8d105d4d77cc973
Fabian <fabian.calangiu@studio.unibo.it>
2026-01-18 14:29:19 +0100

    Add sample view in RogueKongView and impl

--- src/main/java/it/unibo/roguekong/view/RogueKongView.java
+++ src/main/java/it/unibo/roguekong/view/RogueKongView.java
@@ -1,5 +1,14 @@
 package it.unibo.roguekong.view;
 
-public interface RogueKongView {
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 
+/*
+ * This is where the main Scene and Panel are created -- view the implementation
+ * for further information (future edits may change its functionalities)
+ */
+
+public interface RogueKongView {
+    public Scene getScene();
+    public Pane getRoot();
 }
