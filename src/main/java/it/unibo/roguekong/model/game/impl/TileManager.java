c82f5800f951d1cb9f58b819fc026130127d4e11
Menghetz <manuelmenghetti0@gmail.com>
2026-01-19 16:01:31 +0100

    add methods

--- src/main/java/it/unibo/roguekong/model/game/impl/TileManager.java
+++ src/main/java/it/unibo/roguekong/model/game/impl/TileManager.java
@@ -3,16 +3,32 @@ package it.unibo.roguekong.model.game.impl;
 import it.unibo.roguekong.model.value.impl.PositionImpl;
 
 public class TileManager {
-
+    int size = 16; // The size of each pixel
     Tile[] tileSet; // The length of the array depends on how many kind of tiles we are going to use
     int [][] gameMap; // This represents the matrix map. It will be filled
 
     public TileManager(int width, int height, int numTiles) {
         this.tileSet = new Tile[numTiles];
-        this.gameMap = new int[width][height];
+        this.gameMap = new int[height][width];
+
+        Tile tile1 = new Tile("/assets/sprites/wall.png", true, TileType.LADDER);
+        Tile[] tiles = {tile1};
     }
 
-    public void fillTile(){}
+    /**
+     * This is a try
+     */
+    public void fillTile(){
+        for(int i = 0; i < this.gameMap.length; i++){
+            for(int j = 0; j < this.gameMap[0].length; j++){
+                if(i == gameMap.length - 1){
+                    gameMap[i][j] = 1;
+                } else{
+                    gameMap[i][j] = 0;
+                }
+            }
+        }
+    }
 
     public void fillGameMap(){};
 
@@ -26,13 +42,22 @@ public class TileManager {
 
     private boolean checkTileOutOfBounds(int col, int row) {
         if(row >= 0 && row < this.gameMap.length &&
-                col >= 0 && col < this.gameMap[row].length) {}
+                col >= 0 && col < this.gameMap[0].length) {
+            return true;
+        } else{
+            return false;
+        }
     }
 
     public Tile getTileAtPosition(PositionImpl pos) {
-        int col = (int) pos.getX() / tileSet[0].getWidth();
-        int row = (int) pos.getY() / tileSet[0].getHeight();
+        int col = (int) pos.getX() / this.size;
+        int row = (int) pos.getY() / this.size;
 
+        if(checkTileOutOfBounds(col, row)) {
+            int index = gameMap[row][col];
+            return tileSet[index];
+        }
 
+        return null;
     }
 }
