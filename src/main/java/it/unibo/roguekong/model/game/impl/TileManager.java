4d4c7b029313b454686b2eebef79e8e2b9f3da1b
Menghetz <manuelmenghetti0@gmail.com>
2026-01-21 18:30:05 +0100

    Add: map filler algorithm, with a given txt map. Also add a temp-map as a try

--- src/main/java/it/unibo/roguekong/model/game/impl/TileManager.java
+++ src/main/java/it/unibo/roguekong/model/game/impl/TileManager.java
@@ -2,6 +2,11 @@ package it.unibo.roguekong.model.game.impl;
 
 import it.unibo.roguekong.model.value.impl.PositionImpl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class TileManager {
     private final static int ROWS = 20; //The Y of the map
     private final static int COLS = 32; //The X of the map
@@ -10,30 +15,56 @@ public class TileManager {
     private final Tile[] tileSet;
     private final int [][] gameMap;
 
-    public TileManager() {
+    public TileManager(String mapPath) {
         this.gameMap = new int[ROWS][COLS];
+
         Tile tile0 = new Tile("", false, TileType.VOID);
         Tile tile1 = new Tile("/assets/sprites/Sand.png", true, TileType.PLATFORM);
         Tile tile2 = new Tile("/assets/sprites/Wall.png", true, TileType.PLATFORM);
         Tile tile3 = new Tile("/assets/sprites/Dirt.png", true, TileType.PLATFORM);
         Tile tile4 = new Tile("/assets/sprites/Water.png", false, TileType.VOID);
+
         this.tileSet = new Tile[] { tile0, tile1, tile2, tile3, tile4 };
-        this.fillGameMap();
+
+        this.fillGameMap(mapPath);
     }
 
     /**
      * This method fill the matrix, soon it will be passed a file.txt with inside the map
      */
-    public void fillGameMap(){
+    public void fillGameMap(String mapPath){
         // must be complete in the future, when it will be implemented the file reader map
-        for(int i = 0; i < ROWS; i++){
-            for(int j = 0; j < COLS; j++){
-                if(i == ROWS - 1){
-                    gameMap[i][j] = 4;
-                } else{
-                    gameMap[i][j] = 1;
+//        for(int i = 0; i < ROWS; i++){
+//            for(int j = 0; j < COLS; j++){
+//                if(i == ROWS - 1){
+//                    gameMap[i][j] = 4;
+//                } else{
+//                    gameMap[i][j] = 1;
+//                }
+//            }
+//        }
+        try(final InputStream mapFile = ClassLoader.getSystemResourceAsStream(mapPath)) {
+            BufferedReader mapReader = new BufferedReader(new InputStreamReader(mapFile));
+
+            int row = 0;
+            int col = 0;
+
+            while (row < ROWS && col < COLS) {
+                String line = mapReader.readLine();
+
+                while(col < COLS) {
+                    String values[] = line.split(" ");
+                    int n = Integer.parseInt(values[col]);
+                    this.gameMap[row][col] = n;
+                    col++;
+                }
+                if(col == COLS) {
+                    col = 0;
+                    row++;
                 }
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     };
 
--- src/test/java/it/unibo/roguekong/test/LevelTest.java
+++ src/test/java/it/unibo/roguekong/test/LevelTest.java
@@ -24,7 +24,7 @@ public class LevelTest {
         this.player = new PlayerImpl();
         this.spawnPoint = new PositionImpl(0, 0);
         this.endPoint = new PositionImpl(10, 10);
-        this.tileManager = new TileManager();
+        this.tileManager = new TileManager("ciao");
         this.level = new LevelModel(this.spawnPoint, this.endPoint, List.of(), List.of(), this.player, this.tileManager);
     }
 
