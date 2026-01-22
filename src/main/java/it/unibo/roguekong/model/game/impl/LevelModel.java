06257f01ebf6464ca507002b2cc69dbd6948f596
Menghetz <manuelmenghetti0@gmail.com>
2026-01-22 16:14:30 +0100

    Add: gravity method

--- src/main/java/it/unibo/roguekong/model/game/impl/LevelModel.java
+++ src/main/java/it/unibo/roguekong/model/game/impl/LevelModel.java
@@ -7,52 +7,62 @@ import it.unibo.roguekong.model.entity.Player;
 import it.unibo.roguekong.model.game.Level;
 import it.unibo.roguekong.model.value.Position;
 import it.unibo.roguekong.model.value.impl.PositionImpl;
+import it.unibo.roguekong.model.value.impl.VelocityImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LevelModel implements Level {
-    private final List<Enemy> ENEMIES;
-    private final List<GamePlatform> PLATFORMS;
-    private final PositionImpl SPAWN_POSITION;
-    private final PositionImpl END_POINT;
-    private final PlayerImpl PLAYER;
+    private final List<Enemy> enemies;
+    private final List<GamePlatform> platforms;
+    private final PositionImpl spawnPosition;
+    private final PositionImpl endPoint;
+    private final PlayerImpl player;
     private boolean isComplete;
     private final TileManager tileManager;
+    private final int gravity;
 
-    public LevelModel(final PositionImpl spawnPoint, final PositionImpl endPoint, final List<GamePlatform> platforms, final List<Enemy> enemies, final PlayerImpl player, final TileManager tileManager) {
-        this.SPAWN_POSITION = spawnPoint;
-        this.END_POINT = endPoint;
-        this.PLATFORMS = new ArrayList<>(platforms);
-        this.ENEMIES = new ArrayList<>(enemies);
-        this.PLAYER = player;
+    public LevelModel(
+            final PositionImpl spawnPoint,
+            final PositionImpl endPoint,
+            final List<GamePlatform> platforms,
+            final List<Enemy> enemies,
+            final PlayerImpl player,
+            final TileManager tileManager,
+            final int gravity) {
+        this.spawnPosition = spawnPoint;
+        this.endPoint = endPoint;
+        this.platforms = new ArrayList<>(platforms);
+        this.enemies = new ArrayList<>(enemies);
+        this.player = player;
         this.isComplete = false;
         this.tileManager = tileManager;
+        this.gravity = gravity;
     }
 
     @Override
     public List<GamePlatform> getPlatforms() {
-        return List.copyOf(PLATFORMS);
+        return List.copyOf(platforms);
     }
 
     @Override
     public List<Enemy> getEnemies() {
-        return List.copyOf(ENEMIES);
+        return List.copyOf(enemies);
     }
 
     @Override
     public Player getPlayer() {
-        return this.PLAYER;
+        return this.player;
     }
 
     @Override
     public Position getSpawnPoint() {
-        return this.SPAWN_POSITION;
+        return this.spawnPosition;
     }
 
     @Override
     public Position getEndPoint() {
-        return this.END_POINT;
+        return this.endPoint;
     }
 
     @Override
@@ -68,7 +78,8 @@ public class LevelModel implements Level {
     @Override
     public void init() {
         this.isComplete = false;
-        this.PLAYER.setPosition(SPAWN_POSITION.getX(), SPAWN_POSITION.getY());
+        this.player.setPosition(this.spawnPosition.getX(), this.spawnPosition.getY());
+        this.setGravityOnPlayer();
     }
 
     public TileManager getTileManager() {
@@ -76,8 +87,12 @@ public class LevelModel implements Level {
     }
 
     private void checkLevel() {
-        if(this.PLAYER.getPosition().equals(this.END_POINT)){
+        if(this.player.getPosition().equals(this.endPoint)){
             this.isComplete = true;
         }
+    }
+
+    private void setGravityOnPlayer() {
+        this.player.setVelocity(new VelocityImpl(1, 1 * this.gravity));
     }
 }
