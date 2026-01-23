2111439133681e9fd5ee602d44552967c0706be2
Menghetz <manuelmenghetti0@gmail.com>
2026-01-23 17:45:47 +0100

    Add: Drawing first map and add new water tile

--- src/main/java/it/unibo/roguekong/model/game/impl/TileManager.java
+++ src/main/java/it/unibo/roguekong/model/game/impl/TileManager.java
@@ -26,12 +26,13 @@ public class TileManager {
         Tile tile2 = new Tile(Assets.GRASSY_SOIL, true, TileType.PLATFORM);
         Tile tile3 = new Tile(Assets.SOIL, true, TileType.PLATFORM);
         Tile tile4 = new Tile(Assets.WATER, false, TileType.VOID);
+        Tile tile5 = new Tile(Assets.WATER_RIPPLE, false, TileType.VOID);
 
-        Tile tile5 = new Tile(Assets.DARK_CLOUD, false, TileType.VOID);
-        Tile tile6 = new Tile(Assets.DARK_FOGGY_SKY, false, TileType.VOID);
-        Tile tile7 = new Tile(Assets.DARK_SKY, false, TileType.VOID);
+        Tile tile6 = new Tile(Assets.DARK_CLOUD, false, TileType.VOID);
+        Tile tile7 = new Tile(Assets.DARK_FOGGY_SKY, false, TileType.VOID);
+        Tile tile8 = new Tile(Assets.DARK_SKY, false, TileType.VOID);
 
-        this.tileSet = new Tile[] { tile0, tile1, tile2, tile3, tile4, tile5, tile6, tile7 };
+        this.tileSet = new Tile[] { tile0, tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8 };
 
         this.fillGameBackground(backgroundPath);
         this.fillGameMap(mapPath);
--- src/main/java/it/unibo/roguekong/view/paths/Assets.java
+++ src/main/java/it/unibo/roguekong/view/paths/Assets.java
@@ -7,6 +7,7 @@ public final class Assets {
     public static final String GRASSY_SOIL = ROOT + "sprites/grassy_soil.png";
     public static final String SOIL = ROOT + "sprites/soil.png";
     public static final String WATER = ROOT + "sprites/water.png";
+    public static final String WATER_RIPPLE = ROOT + "sprites/water_ripples.png";
     public static final String DARK_CLOUD = ROOT + "backgrounds/dark_cloud.png";
     public static final String DARK_FOGGY_SKY = ROOT + "backgrounds/dark_foggy_sky.png";
     public static final String DARK_SKY = ROOT + "backgrounds/dark_sky.png";
