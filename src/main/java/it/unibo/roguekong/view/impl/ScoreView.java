24ffeed8b2679a149b6363abc40e59aada9be59d
Fabian <fabian.calangiu@studio.unibo.it>
2026-01-28 15:20:51 +0100

    Add format and method to set the scores in the score scene

--- src/main/java/it/unibo/roguekong/view/impl/ScoreView.java
+++ src/main/java/it/unibo/roguekong/view/impl/ScoreView.java
@@ -1,10 +1,13 @@
 package it.unibo.roguekong.view.impl;
 
+import it.unibo.roguekong.model.game.impl.ScoreRecord;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class ScoreView {
     private final Scene scene;
@@ -41,7 +44,19 @@ public class ScoreView {
         );
     }
 
+    private String format(List<ScoreRecord> scores, int index){
+        if(scores.size() <= index){
+            return "-";
+        }
+        ScoreRecord s = scores.get(index);
+        return s.name() + " - " + s.score();
+    }
 
+    public void setScores(List<ScoreRecord> scores){
+        score1.setText(format(scores, 0));
+        score2.setText(format(scores, 1));
+        score3.setText(format(scores, 2));
+    }
 
     public Scene getScene() {
         return scene;
