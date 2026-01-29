1ab4d0d162ea79473dd10158b466a64c3fa48f73
Fabian <fabian.calangiu@studio.unibo.it>
2026-01-29 10:01:09 +0100

    Add portal assets

--- src/main/java/it/unibo/roguekong/view/paths/Assets.java
+++ src/main/java/it/unibo/roguekong/view/paths/Assets.java
@@ -12,6 +12,10 @@ public final class Assets {
     public static final String TOP_TREE_BUSH = ROOT + "sprites/top_tree_bush.png";
     public static final String MIDDLE_TREE_BUSH = ROOT + "sprites/middle_tree_bush.png";
     public static final String TREE_LOG = ROOT + "sprites/tree_log.png";
+    public static final String UPPER_PURPLE_PORTAL = ROOT + "sprites/upper_purple_portal.png";
+    public static final String LOWER_PURPLE_PORTAL = ROOT + "sprites/lower_purple_portal.png";
+    public static final String UPPER_ORANGE_PORTAL = ROOT + "sprites/upper_orange_portal.png";
+    public static final String LOWER_ORANGE_PORTAL = ROOT + "sprites/lower_orange_portal.png";
     public static final String DARK_CLOUD = ROOT + "backgrounds/dark_cloud.png";
     public static final String DARK_FOGGY_SKY = ROOT + "backgrounds/dark_foggy_sky.png";
     public static final String DARK_SKY = ROOT + "backgrounds/dark_sky.png";
